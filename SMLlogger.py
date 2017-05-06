fe92db5914f5554f7dd4956f540d8e4f7d094971
dirkc <dirkclemens@users.noreply.github.com>
2017-05-06 12:07:39 +0200

    Update SMLlogger.py
    improvements, range check while converting hex to int

--- SMLlogger.py
+++ SMLlogger.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 ########################################################################
 #
 #   SMLlogger:  
-#		7.2.2016 Dirk Clemens 
+#		7.2.2016 Dirk Clemens 	iot@adcore.de
+#		6.5.2017 Dirk Clemens	improvements, range check while converting hex to int
 #		read data using a IR-USB-Head from a SML-counter (OBIS)
 #		tested with "Zweirichtungszähler eHZ-IW8E2Axxx"
 #	
@@ -13,17 +14,17 @@
 #		http://wiki.volkszaehler.org/hardware/channels/meters/power/edl-ehz/emh-ehz-h1
 #		http://volkszaehler.org/pipermail/volkszaehler-users/2012-September/000451.html
 #		http://wiki.volkszaehler.org/software/sml
-#		http://www.mscons.net/obis_kennzahlen.pdf
+#		https://sharepoint.infra-fuerth.de/unbundling/obis_kennzahlen.pdf
 #		https://www.mikrocontroller.net/attachment/89888/Q3Dx_D0_Spezifikation_v11.pdf
 #		https://eclipse.org/paho/clients/python/ 
 #
 #	requirements:
 #	sudo apt-get install python-dev python-pip python-serial python3-serial 
+#	sudo pip install RPi.GPIO
 #	sudo pip install paho-mqtt
 #
 ########################################################################
 
-from __future__ import print_function 
 import sys
 import os
 import serial
@@ -32,54 +33,82 @@ import logging
 from datetime import datetime
 import math
 import paho.mqtt.client as paho
+import ssl
+import urllib
 
-# MQTT Callback events #
-# connection event
-def on_connect(client, data, flags, rc):
-	print('Connected, rc: ' + str(rc))
-# subscription event
-def on_subscribe(client, userdata, mid, gqos):
-	print('Subscribed: ' + str(mid))
-# received message event
-def on_message(client, obj, msg):
-	print(msg.topic+" "+str(msg.payload))
+# ------------- #
+# settings      #
+# ------------- #
 
-# hex string to signed integer 32 bit
-# unfortunately this function is not correct, but the best one I found
-def hexstr2signed32int(hexstr):
-	num = long(hexstr, 16)
-	if num >> 31: 
-		num = num - 2**32 
-	if num > -100000000 and num < 100000000: # ok, this is not good ;-)
-		return num
-	else:
-		logging.debug("%s %s" % (hexstr, num))
-	
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser  # ver. < 3.0
+
+config = ConfigParser()
+config.read('/root/.credentials/pycredentials') 
+
+MQTT_SERVER = config['DEFAULT']['mqtt_server']    
+MQTT_PORT = config['DEFAULT']['mqtt_port']    
+MQTT_TLS =  config['DEFAULT']['mqtt_tls']
+MQTT_SERVERIP = config['DEFAULT']['mqtt_serverip']
+MQTT_CACERT = config['DEFAULT']['mqtt_cacert']
+
+# hex string to signed integer, inkl. range check http://stackoverflow.com/a/6727975 
+def hexstr2signedint(hexval):
+	uintval = int(hexval,16)
+	if uintval > 0x7FFFFFFF:		# > 2147483647
+		uintval -= 0x100000000		# -= 4294967296 
+	return uintval
+
 # parse hex string from USB serial stream and extract values for obis_id
 # print result and publish as mqtt message 
-def parseSML(data_hex, obis_id, obis_string, pos, len):
+def parseSML(data_hex, obis_id, obis_string, pos, length):
 	obis_value = 0
 	# find position of OBIS-Kennzahl 
 	position = data_hex.find(obis_string)
 	
 	# break, do not send mqtt message
-	if position == 0: 
+	if position <= 0:
 		return 0 
 
-	# extract reading from position start: 34 lenght: 10 (for 1.8.0.)
-	hex_value = data_hex[position+pos:position+pos+len]
+	# extract reading from position start: 34 length: 10 (for 1.8.0.)
+	hex_value = data_hex[position+pos:position+pos+length]
 	
-	# convert to integer
-	obis_value = hexstr2signed32int(hex_value)/10.0
-
-	# publish value as mosquitto message
-	mqtt_topic = "smarthome/vzlogger/%s/state" % (obis_id,)
-	#print (mqtt_topic)
-	client.publish(mqtt_topic, obis_value )
+	# convert to integer, check range  
+	obis_value = hexstr2signedint(hex_value)
+	
+	# # publish value as mosquitto message
+	mqtt_topic = "smarthome/SMLlogger/%s/state" % (obis_id,)
+	# #print (mqtt_topic)
+	client.publish(mqtt_topic, str(obis_value/10.0) )
 	
 	# wait ...
 	time.sleep(1)	
 	return obis_value
+
+########################################################################
+import sys
+import glob
+import serial
+# http://stackoverflow.com/a/14224477
+def checkUSB():
+	if sys.platform.startswith('linux'):
+		# this excludes your current terminal "/dev/tty"
+		ports = glob.glob('/dev/ttyUSB*')
+	else:
+		raise EnvironmentError('Unsupported platform')
+		pushover(e)
+	result = []
+	for port in ports:
+		try:
+			s = serial.Serial(port)
+			s.close()
+			result.append(port)
+		except (OSError, serial.SerialException):
+			pass
+	return result
+
 
 ########################################################################
 # MAIN  	
@@ -90,7 +119,7 @@ def main():
 
 	# logging
 	global logging
-	logging.basicConfig(filename='/log/vzlogger.log', 
+	logging.basicConfig(filename='/var/log/SMLlogger.log', 
 					level=logging.DEBUG, 
 					format='%(asctime)s %(message)s', 
 					datefmt='%Y-%m-%d %H:%M:%S')
@@ -98,21 +127,32 @@ def main():
 	#logger = logging.getLogger(__name__)
 	#logger.setLevel(logging.DEBUG)
 
+	usbports = checkUSB()
+	# print len(ports)
+	if (len(usbports) == 1):
+		#print(usbports[0])
+		print("opening USB port :" + usbports[0])	
+	else:
+		print("SMLlogger: usbports: %s ports" % (len(usbports), ))
+		pushover("SMLlogger: usbports: %s ports" % (len(usbports), ))
+		sys.exit(1)
+
 	# eHZ-Datentelegramme können mittels eines optischen Auslesekopfs nach DIN EN 62056-21 
 	# z. B. über die serielle Schnittstelle eines PC ausgelesen werden.
 	# Einstellung: bit/s= 9600, Datenbit = 7, Parität = gerade, Stoppbits = 1, Flusssteuerung = kein.
 	ser = serial.Serial(
-		port='/dev/ttyUSB0', 
+		port=usbports[0], #'/dev/ttyUSB1', 
 		baudrate=9600, 
-		bytesize=8, 
-		parity='N', 
-		stopbits=1, 
+		parity=serial.PARITY_NONE,
+		stopbits=serial.STOPBITS_ONE,
+		bytesize=serial.EIGHTBITS,
 		timeout=2, 
 		xonxoff=False, 
 		rtscts=False, 
 		dsrdtr=False)
 	ser.flushInput()
 	ser.flushOutput()
+
 
 	data_hex = ''
 	reading_ok = False
@@ -137,25 +177,31 @@ def main():
 	# print (data_hex)
 
 	if reading_ok:
+		############################################
+		# error logging
+		#
 		__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-		file = open(os.path.join(__location__, "vzlogger.txt"), "a")		
+		file = open(os.path.join(__location__, "energy.logger.txt"), "a")		
 		tstamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
 		# tested with eHZ-IW8E2Axxx
 		sml180 = parseSML(data_hex, "180", '070100010800ff', 34, 10) 
-		file.write("%s;180;%s\n" % (tstamp, sml180))
+		if (sml180 == 0):
+			logging.debug("%s int:%s raw:%s" % ("180", sml180, data_hex))
+			file.write("%s;180;%s;%s\n" % (tstamp, sml180, data_hex))
 		print ("180: ", sml180)
 		
 		sml280 = parseSML(data_hex, "280", '070100020800ff', 34, 10)
-		file.write("%s;280;%s\n" % (tstamp, sml280))
+		if (sml280 == 0):
+			logging.debug("%s int:%s raw:%s" % ("280", sml280, data_hex))
+			file.write("%s;280;%s;%s\n" % (tstamp, sml280, data_hex))
 		print ("280: ", sml280) 
 		
 		sml167 = parseSML(data_hex, "167", '070100100700ff', 28, 8) 
-		file.write("%s;167;%s\n" % (tstamp, sml167))
+		if (sml167 == 0):
+			logging.debug("%s int:%s raw:%s" % ("167", sml167, data_hex))
+			file.write("%s;167;%s;%s\n" % (tstamp, sml167, data_hex))
 		print ("167: ", sml167)
-		
-		file.close()
-
 
 ########################################################################
 #
@@ -165,15 +211,15 @@ if __name__ == "__main__":
 
 	# create the MQTT client
 	client = paho.Client()
-	# assign event callbacks
-	#client.on_connect = on_connect
-	#client.on_message = on_message
-	# client connection to MQTT server address
-	client.connect("smarthome", 1883, 60)	
+	if (MQTT_TLS == "True"):
+		print("connecting using tls")
+		#client.tls_set(MQTT_CACERT)
+		client.tls_set(ca_certs="/home/chip/.ssh/ca.crt")
+		#client.tls_insecure_set(True)     # prevents error - ssl.SSLError: Certificate subject does not match remote hostname.
+	client.connect(MQTT_SERVER, int(MQTT_PORT), 60)  	
 
 	main()
 	
 	client.disconnect()
-
 
 
